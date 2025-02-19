a8cedb14f7e76b22036632f8dc4bcc40eb7c100b
SaiAdithya3 <saiadithyakancharla@gmail.com>
2025-02-19 10:47:04 +0530

    fixes

---
+++ src/components/DefiUnlocked/DefiUnlocked.tsx
@@ -0,0 +1,10 @@
+export default function DefiUnlocked() {
+  return (
+    <div className="w-full flex flex-col items-center justify-center pt-[100px] ">
+      <div className="w-full max-w-[1024px] flex gap-[72px] items-start pb-[100px]">
+        defi unlocked
+      </div>
+      <div className="h-[1px] w-full max-w-[1024px] bg-zinc-100"></div>
+    </div>
+  );
+}
